220f1ed290700af1a0c68b0ceac0554c3e279b6c
SanidhyaJoshi69 <joshisanidhya35@gmail.com>
2026-02-12 15:53:55 +0530

    Add files via upload

---
+++ P1.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter a number \n");
+    scanf("%d%d",&a, &b);
+    if (a==b) {
+        printf("THE TWO NOS ARE EQUAL");}
+    else{
+        printf("THE TWO NOS ARE NOT-EQUAL"); }
+    return 0;}
