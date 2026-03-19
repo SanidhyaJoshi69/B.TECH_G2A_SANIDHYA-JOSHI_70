1626be8b30ff3e4a7f06a2625fcd7a40d5d189c8
SanidhyaJoshi69 <joshisanidhya35@gmail.com>
2026-03-19 13:49:15 +0530

    Add files via upload

---
+++ asg14.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n, even = 0, odd = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+        if(i % 2 == 0)
+            even += i;
+        else
+            odd += i;
+    }
+
+    printf("Sum of Even = %d\n", even);
+    printf("Sum of Odd = %d", odd);
+
+    return 0;
+}
